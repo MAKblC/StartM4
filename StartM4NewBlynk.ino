8cfc1d0abb015dd94e843bfbca47b7fe59d7e935
Max <41009925+MAKblC@users.noreply.github.com>
2023-12-12 14:57:26 +0300

    Update StartM4NewBlynk.ino

--- StartM4NewBlynk.ino
+++ StartM4NewBlynk.ino
@@ -112,8 +112,12 @@ void setup()
 
   setBusChannel(0x07);
   bool bme_status = bme280.begin();
-  if (!bme_status)
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  if (!bme_status) {
+    Serial.println("Не найден по адресу 0х77, пробую другой...");
+    bme_status = bme280.begin(0x76);
+    if (!bme_status)
+      Serial.println("Датчик не найден, проверьте соединение");
+  }
 
   timer_update.setInterval(1000, readSendData); // настройка таймера с параметрами (период обновления, вызываемая функция)
 }

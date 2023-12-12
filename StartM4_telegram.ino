95f23a1750fe4d5b49f54c8bb52e045b4a05325b
Max <41009925+MAKblC@users.noreply.github.com>
2023-12-12 14:57:46 +0300

    Update StartM4_telegram.ino

--- StartM4_telegram.ino
+++ StartM4_telegram.ino
@@ -124,8 +124,12 @@ void setup()
 
   setBusChannel(0x07);
   bool bme_status = bme280.begin();
-  if (!bme_status)
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  if (!bme_status) {
+    Serial.println("Не найден по адресу 0х77, пробую другой...");
+    bme_status = bme280.begin(0x76);
+    if (!bme_status)
+      Serial.println("Датчик не найден, проверьте соединение");
+  }
 
 }
 
